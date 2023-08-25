67bc21315d8d2dcca99d0d6a38481bcbbf0cd498
mathiasmantai <mmantaibusiness@gmail.com>
2023-08-25 12:57:00 +0200

    feat: functional routing with arguments

--- src/Router.php
+++ src/Router.php
@@ -9,17 +9,24 @@ class Router
     
     public array $RouteCollection;
 
-    public function registerRoute(string $route, callable $callback)
+    public function registerRoute(string $route, callable $callback): void
     {   
+        $parsedRoute = $this->parseRoute($route);
         $this->RouteCollection[] = [
-            "route" => $route,  
+            ...$parsedRoute,  
             "callback" => $callback
         ];
     }
 
-    public function executeRoute(string $route, array $arguments)
+    public function executeRoute(string $route)
     {
-        $foundRoute = $this->findRouteInCollection($route);
+        //find a matching route
+        $foundRoute = $this->findMatchingRoute($route);
+
+        //get argument part of route string
+        $argumentString = str_replace($foundRoute["route"], "", $route);
+
+        $arguments = $this->parseArguments($argumentString);
 
         if($foundRoute != null)
         {
@@ -31,11 +38,42 @@ class Router
         }
     }
 
+    public function findMatchingRoute(string $route)
+    {
+        //find matching route from the route collection array
+        $foundRoute = null;
+        $run = true;
+
+        while($run)
+        {
+            $foundRoute = $this->findRouteInCollection($route);
+
+            if(($foundRoute == null && $route == "/") || $foundRoute != null)
+            {
+                $run = false;
+            }
+            else 
+            {
+                $this->deconstructRoute($route);
+            }
+        }
+
+        return $foundRoute;
+    }
+
+    public function deconstructRoute(string &$route)
+    {
+        //remove last basename from route
+        $routeParts = explode("/", $route);
+        array_pop($routeParts);
+        $route = implode("/", $routeParts);
+    }
+
     public function findRouteInCollection(string $searchString)
     {
         foreach($this->RouteCollection as $route)
         {
-            if(str_contains($route["route"], $searchString))
+            if($route["route"] == $searchString)
             {
                 return $route;
             }
@@ -49,7 +87,7 @@ class Router
         //parse route and return an array containing the main route + variables
         $parsedRoute = [
             "route" => "",
-            "variables" => []
+            "arguments" => []
         ];
 
         $routeParts = explode("/", $route);
@@ -58,7 +96,7 @@ class Router
         {
             if(str_contains($routePart, "{") && str_contains($routePart, "}"))
             {
-                array_push($parsedRoute["variables"], $routePart);
+                array_push($parsedRoute["arguments"], $routePart);
             }
             else 
             {
@@ -71,5 +109,21 @@ class Router
         $parsedRoute["route"] = $actualRoute;
         
         return $parsedRoute;
+    }
+
+    public function parseArguments(string $argumentString)
+    {
+        $argumentParts = explode("/", $argumentString);
+
+        $arguments = [];
+        foreach($argumentParts as $argument)
+        {
+            if(trim($argument) != "")
+            {
+                array_push($arguments, $argument);
+            }
+        }
+
+        return $arguments;
     }
 }
