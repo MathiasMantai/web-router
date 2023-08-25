dbd28282bfccdd4c94d0efae29742f23a1edbbed
mathiasmantai <mmantaibusiness@gmail.com>
2023-08-25 11:39:37 +0200

    feat. route parsing

--- src/Router.php
+++ src/Router.php
@@ -11,4 +11,33 @@ class Router
     {   
         $this->RouteCollection[$path] = $callback;
     }
+
+    public function parseRoute(string $route)
+    {
+        //parse route and return an array containing the main route + variables
+        $parsedRoute = [
+            "route" => "",
+            "variables" => []
+        ];
+
+        $routeParts = explode("/", $route);
+        $actualRouteParts = [];
+        foreach($routeParts as $routePart)
+        {
+            if(str_contains($routePart, "{") && str_contains($routePart, "}"))
+            {
+                array_push($parsedRoute["variables"], $routePart);
+            }
+            else 
+            {
+                array_push($actualRouteParts, $routePart);
+            }
+        }
+
+        //join the actualRoute
+        $actualRoute = implode("/", $actualRouteParts);
+        $parsedRoute["route"] = $actualRoute;
+        
+        return $parsedRoute;
+    }
 }
