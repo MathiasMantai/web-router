c6c0f4c7fe4e1c271579b6e6d2c22707c1ec8cb3
mathiasmantai <mmantaibusiness@gmail.com>
2023-08-25 11:58:16 +0200

    feat: find and execute callback of found route

---
+++ src/RouteNotFoundException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace UrlParser;
+use Exception;
+use Throwable;
+
+class RouteNotFoundException extends Exception
+{
+    public function __construct($message = "No matching Route Found", $code = 0, Throwable $previous = null) 
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
--- src/Router.php
+++ src/Router.php
@@ -2,14 +2,46 @@
 
 namespace UrlParser;
 
+use UrlParser\RouteNotFoundException;
+
 class Router 
 {
     
     public array $RouteCollection;
 
-    public function registerRoute(string $path, callable $callback)
+    public function registerRoute(string $route, callable $callback)
     {   
-        $this->RouteCollection[$path] = $callback;
+        $this->RouteCollection[] = [
+            "route" => $route,  
+            "callback" => $callback
+        ];
+    }
+
+    public function executeRoute(string $route, array $arguments)
+    {
+        $foundRoute = $this->findRouteInCollection($route);
+
+        if($foundRoute != null)
+        {
+            $foundRoute["callback"](...$arguments);
+        }
+        else
+        {
+            throw new RouteNotFoundException();
+        }
+    }
+
+    public function findRouteInCollection(string $searchString)
+    {
+        foreach($this->RouteCollection as $route)
+        {
+            if(str_contains($route["route"], $searchString))
+            {
+                return $route;
+            }
+        }
+
+        return null;
     }
 
     public function parseRoute(string $route)
